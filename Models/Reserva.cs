5f9ff7eaff6d5911422780c6607c3eda0c9a6078
Leonardo <leolds010101@gmail.com>
2024-05-10 11:15:49 -0300

    project

---
+++ Models/Reserva.cs
@@ -0,0 +1,57 @@
+﻿using Hospedagem.Models;
+
+namespace Hospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade <= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Número de hóspedes maior que a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+        public int ObterQuantidadeDeDias()
+        {
+            return DiasReservados;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = valor * 0.10M;
+                valor = valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Hospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+Console.WriteLine("Quantos dias serão de reserva?");
+int diasReservados = Convert.ToInt16(Console.ReadLine());
+
+Reserva reserva = new Reserva(diasReservados: diasReservados);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Dias: {reserva.ObterQuantidadeDeDias()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
